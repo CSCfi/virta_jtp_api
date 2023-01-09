7ebd0dfa394f162dc24edc7d49eb8be26c0c7e73
Ville Hämäläinen <ville.hamalainen@csc.fi>
2023-01-09 10:39:03 +0200

    tilanneraportti new columns

--- julkaisu-api/src/main/java/fi/csc/virta/julkaisut/domain/Tilanneraportti.java
+++ julkaisu-api/src/main/java/fi/csc/virta/julkaisut/domain/Tilanneraportti.java
@@ -24,6 +24,21 @@ public class Tilanneraportti implements DTO {
     private Integer julkaisunTila;
     private String jufoTunnus;
     private String jufoLuokkaKoodi;
+	
+	private BigDecimal avoinSaatavuusJulkaisumaksu;
+	private Integer avoinSaatavuusJulkaisumaksuVuosi;
+	private String julkaisuKanavaOA;
+	private String avoinSaatavuusKytkin;
+	private String lisenssiKoodi;
+	private String muotoKoodi;
+	private String yleisoKoodi;
+	private String emojulkaisuntyyppiKoodi;
+	private String artikkelityyppiKoodi;
+	private String vertaisarvioituKytkin;
+	private String raporttiKytkin;
+	private String opinnayteKoodi;
+	private String taidetyyppiKoodi;
+	private String avSovellusTyyppiKoodi;
 
 
     public String getOrganisaatioNimi() {
@@ -82,6 +97,48 @@ public class Tilanneraportti implements DTO {
         return jufoLuokkaKoodi;
     }
 
+	public BigDecimal avoinSaatavuusJulkaisumaksu {
+		return avoinSaatavuusJulkaisumaksu;
+    }
+	public Integer avoinSaatavuusJulkaisumaksuVuosi {
+		return avoinSaatavuusJulkaisumaksuVuosi;
+    }
+	public String julkaisuKanavaOA {
+		return julkaisuKanavaOA;
+    }
+	public String avoinSaatavuusKytkin {
+		return avoinSaatavuusKytkin;
+    }
+	public String lisenssiKoodi {
+		return lisenssiKoodi;
+    }
+	public String muotoKoodi {
+		return muotoKoodi;
+    }
+	public String yleisoKoodi {
+		return yleisoKoodi;
+    }
+	public String emojulkaisuntyyppiKoodi {
+		return emojulkaisuntyyppiKoodi;
+    }
+	public String artikkelityyppiKoodi {
+		return artikkelityyppiKoodi;
+    }
+	public String vertaisarvioituKytkin {
+		return vertaisarvioituKytkin;
+    }
+	public String raporttiKytkin {
+		return raporttiKytkin;
+    }
+	public String opinnayteKoodi {
+		return opinnayteKoodi;
+    }
+	public String taidetyyppiKoodi {
+		return taidetyyppiKoodi;
+    }
+	public String avSovellusTyyppiKoodi {
+		return avSovellusTyyppiKoodi;
+    }
 
     public void setOrganisaatioNimi(String organisaatioNimi) {
         this.organisaatioNimi = organisaatioNimi;
@@ -137,6 +194,49 @@ public class Tilanneraportti implements DTO {
     
     public void setJufoLuokkaKoodi(String jufoLuokkaKoodi) {
         this.jufoLuokkaKoodi = jufoLuokkaKoodi;
+    }
+	
+	public void setAvoinSaatavuusJulkaisumaksu(BigDecimal avoinSaatavuusJulkaisumaksu) {
+		this.avoinSaatavuusJulkaisumaksu = avoinSaatavuusJulkaisumaksu;
+    }
+	public void setAvoinSaatavuusJulkaisumaksuVuosi(Integer avoinSaatavuusJulkaisumaksuVuosi) {
+		this.avoinSaatavuusJulkaisumaksuVuosi = avoinSaatavuusJulkaisumaksuVuosi;
+    }
+	public void setJulkaisuKanavaOA(String julkaisuKanavaOA) {
+		this.julkaisuKanavaOA = julkaisuKanavaOA;
+    }
+	public void setAvoinSaatavuusKytkin(String avoinSaatavuusKytkin) {
+		this.avoinSaatavuusKytkin = avoinSaatavuusKytkin;
+    }
+	public void setLisenssiKoodi(String lisenssiKoodi) {
+		this.lisenssiKoodi = lisenssiKoodi;
+    }
+	public void setMuotoKoodi(String muotoKoodi) {
+		this.muotoKoodi = muotoKoodi;
+    }
+	public void setYleisoKoodi(String yleisoKoodi) {
+		this.yleisoKoodi = yleisoKoodi;
+    }
+	public void setEmojulkaisuntyyppiKoodi(String emojulkaisuntyyppiKoodi) {
+		this.emojulkaisuntyyppiKoodi = emojulkaisuntyyppiKoodi;
+    }
+	public void setArtikkelityyppiKoodi(String artikkelityyppiKoodi) {
+		this.artikkelityyppiKoodi = artikkelityyppiKoodi;
+    }
+	public void setVertaisarvioituKytkin(String vertaisarvioituKytkin) {
+		this.vertaisarvioituKytkin = vertaisarvioituKytkin;
+    }
+	public void setRaporttiKytkin(String raporttiKytkin) {
+		this.raporttiKytkin = raporttiKytkin;
+    }
+	public void setOpinnayteKoodi(String opinnayteKoodi) {
+		this.opinnayteKoodi = opinnayteKoodi;
+    }
+	public void setTaidetyyppiKoodi(String taidetyyppiKoodi) {
+		this.taidetyyppiKoodi = taidetyyppiKoodi;
+    }
+	public void setAVSovellusTyyppiKoodi(String avSovellusTyyppiKoodi) {
+		this.avSovellusTyyppiKoodi = avSovellusTyyppiKoodi;
     }
 
 
