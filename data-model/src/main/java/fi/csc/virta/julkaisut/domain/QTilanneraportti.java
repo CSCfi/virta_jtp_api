cae7c88d246e98c5b6ae7bbd9333af23d7e5a026
villehoo <ville.hamalainen@csc.fi>
2023-01-10 15:16:34 +0200

    add new columns

--- data-model/src/main/java/fi/csc/virta/julkaisut/domain/QTilanneraportti.java
+++ data-model/src/main/java/fi/csc/virta/julkaisut/domain/QTilanneraportti.java
@@ -40,6 +40,20 @@ public class QTilanneraportti extends com.querydsl.sql.RelationalPathBase<QTilan
         public final StringPath jufoTunnus = createString("JufoTunnus");
         public final StringPath jufoLuokkaKoodi = createString("JufoLuokkaKoodi");
         public final StringPath avoinSaatavuusJulkaisumaksu = createString("avoinSaatavuusJulkaisumaksu");
+        public final NumberPath<Integer> avoinSaatavuusJulkaisumaksu = createString("avoinSaatavuusJulkaisumaksu");
+        public final StringPath avoinSaatavuusJulkaisumaksuVuosi = createString("avoinSaatavuusJulkaisumaksuVuosi	");
+        public final StringPath julkaisuKanavaOA = createString("julkaisuKanavaOA");
+        public final StringPath avoinSaatavuusKytkin = createString("avoinSaatavuusKytkin");
+        public final StringPath lisenssiKoodi = createString("lisenssiKoodi");
+        public final StringPath muotoKoodi = createString("muotoKoodi");
+        public final StringPath yleisoKoodi = createString("yleisoKoodi");
+        public final StringPath emojulkaisuntyyppiKoodi = createString("emojulkaisuntyyppiKoodi");
+        public final StringPath artikkelityyppiKoodi = createString("artikkelityyppiKoodi");
+        public final StringPath vertaisarvioituKytkin = createString("vertaisarvioituKytkin");
+        public final StringPath raporttiKytkin = createString("raporttiKytkin");
+        public final StringPath opinnayteKoodi = createString("opinnayteKoodi");
+        public final StringPath taidetyyppiKoodi = createString("taidetyyppiKoodi");
+        public final StringPath	avSovellusTyyppiKoodi = createString("avSovellusTyyppiKoodi");
 
 
     public QTilanneraportti(String variable) {
@@ -78,8 +92,20 @@ public class QTilanneraportti extends com.querydsl.sql.RelationalPathBase<QTilan
 	addMetadata(julkaisunTila, ColumnMetadata.named("JulkaisunTila").withIndex(8).ofType(Types.INTEGER).withSize(10));
         addMetadata(jufoTunnus, ColumnMetadata.named("JufoTunnus").withIndex(13).ofType(Types.NVARCHAR).withSize(10));
         addMetadata(jufoLuokkaKoodi, ColumnMetadata.named("JufoLuokkaKoodi").withIndex(14).ofType(Types.NVARCHAR).withSize(1));
-        addMetadata(avoinSaatavuusJulkaisumaksu, ColumnMetadata.named("AvoinSaatavuusJulkaisumaksu").withIndex(15).ofType(Types.NVARCHAR).withSize(40));
-
+        addMetadata(avoinSaatavuusJulkaisumaksu, ColumnMetadata.named("AvoinSaatavuusJulkaisumaksu").withIndex(15).ofType(Types.NVARCHAR).withSize(30));
+        addMetadata(avoinSaatavuusJulkaisumaksuVuosi, ColumnMetadata.named("avoinSaatavuusJulkaisumaksuVuosi").withIndex(16).ofType(Types.NVARCHAR).withSize(1));
+        addMetadata(julkaisuKanavaOA, ColumnMetadata.named("julkaisuKanavaOA").withIndex(17).ofType(Types.INTEGER).withSize(10));
+        addMetadata(avoinSaatavuusKytkin, ColumnMetadata.named("avoinSaatavuusKytkin").withIndex(18).ofType(Types.NVARCHAR).withSize(1));
+        addMetadata(lisenssiKoodi, ColumnMetadata.named("lisenssiKoodi").withIndex(19).ofType(Types.NVARCHAR).withSize(1));
+        addMetadata(muotoKoodi, ColumnMetadata.named("muotoKoodi").withIndex(20).ofType(Types.NVARCHAR).withSize(1));
+        addMetadata(yleisoKoodi, ColumnMetadata.named("yleisoKoodi").withIndex(21).ofType(Types.NVARCHAR).withSize(1));
+        addMetadata(emojulkaisuntyyppiKoodi, ColumnMetadata.named("emojulkaisuntyyppiKoodi").withIndex(22).ofType(Types.NVARCHAR).withSize(1));
+        addMetadata(artikkelityyppiKoodi, ColumnMetadata.named("artikkelityyppiKoodi").withIndex(23).ofType(Types.NVARCHAR).withSize(1));
+        addMetadata(vertaisarvioituKytkin, ColumnMetadata.named("vertaisarvioituKytkin").withIndex(24).ofType(Types.NVARCHAR).withSize(1));
+        addMetadata(raporttiKytkin, ColumnMetadata.named("raporttiKytkin").withIndex(25).ofType(Types.NVARCHAR).withSize(1));
+        addMetadata(opinnayteKoodi, ColumnMetadata.named("opinnayteKoodi").withIndex(26).ofType(Types.NVARCHAR).withSize(1));
+        addMetadata(taidetyyppiKoodi, ColumnMetadata.named("taidetyyppiKoodi").withIndex(27).ofType(Types.NVARCHAR).withSize(1));
+        addMetadata(avSovellusTyyppiKoodi, ColumnMetadata.named("avSovellusTyyppiKoodi").withIndex(28).ofType(Types.NVARCHAR).withSize(1));
     }
 
 }
